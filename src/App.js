92df8808bc286a58c8bf191379b0041422adeb8f
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-11 20:19:54 -0500

    todo list app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState }  from 'react'; // react manages state, not app, and i want to store todos to render
+import React, { useState, useRef }  from 'react'; // react manages state, not app, and i want to store todos to render
 import TodoList from './TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([]) // when website first loads, we have an empty state
+  // destructure array
+  const [todos, setTodos] = useState([{id:1, name: "ur mom", complete: true}]) // when website first loads, we have an empty state
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return ( 
     <> {/* wrap return in empty <> so that i only return one */}
-    <TodoList /> {/* // i can basically create my own component, so now todo list.js */}
-    <input type="text" />
-    <button>add todo</button>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>add todo</button>
     <button>clear completed</button>
     <div> 0 left to do lol </div>
+    <br></br>
+
+    <TodoList todos={todos} /> {/* // i can basically create my own component, so now todo list.js */}
     </>
   );
 }
