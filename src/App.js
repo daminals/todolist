ed18a4c6d6ca48bec421b5a2ebfe9f112a2b5463
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-11 20:00:39 -0500

    epic reaction bestie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState }  from 'react'; // react manages state, not app, and i want to store todos to render
+import TodoList from './TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [todos, setTodos] = useState([]) // when website first loads, we have an empty state
+  return ( 
+    <> {/* wrap return in empty <> so that i only return one */}
+    <TodoList /> {/* // i can basically create my own component, so now todo list.js */}
+    <input type="text" />
+    <button>add todo</button>
+    <button>clear completed</button>
+    <div> 0 left to do lol </div>
+    </>
   );
 }
 
